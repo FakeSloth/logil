d442ee280327604cf39197d16629e8bc83f6565c
Logan Saunders <timandmoby@gmail.com>
2015-06-30 00:03:58 -0700

    Removed loadNpmTasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,8 +38,6 @@ module.exports = function(grunt) {
     }
 
   });
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('build', ['sass']);
 
